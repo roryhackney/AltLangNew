5b0e2e54958c6573defbae0f8d848024999ec787
Rory Hackney <roryhackney@gmail.com>
2024-05-26 11:56:51 -0700

    file processing done, still to clean

---
+++ Cell.cs
@@ -0,0 +1,18 @@
+public class Cell {
+    private string? Oem {get; set;}
+    private string Model {get; set;}
+    private int LaunchAnnounced {get; set;}
+    private string LaunchStatus {get; set;}
+    private string BodyDimensions {get; set;}
+    private float BodyWeight {get; set;}
+    private string BodySim {get; set;}
+    private string DisplayType {get; set;}
+    private float DisplaySize {get; set;}
+    private string DisplayResolution {get; set;}
+    private string FeaturesSensors {get; set;}
+    private string PlatformOS {get; set;}
+
+    public Cell(string[] values) {
+    
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+static void Main(string[] args) {
+    Dictionary<int, Cell> dict = new Dictionary<int, Cell>();
+    //step one: read the file
+    IEnumerable<string> rows = File.ReadLines("cells.csv");
+    int lineNum = 1;
+    //step two: create the Cell objects
+    foreach (string row in rows) {
+        if (lineNum > 1) {
+            string?[] origs = new string[12];
+
+            int begin = 0;
+            int length = 0;
+            for (int index = 0; index < 12; index++) {
+                //get the next field
+                origs[index] = getNext(row, begin);
+                //if it was in quotes, add 2 to the length, plus 1 for the comma seperator
+                if (row.IndexOf("\"", begin) == begin) length = 3;
+                else length = 1;
+                begin += length;
+                if (origs[index] != null) begin += origs[index].Length;
+            }            
+            // Console.WriteLine(row);
+            Console.WriteLine("[");
+            foreach (string item in origs) {Console.WriteLine(item + "---");}
+            Console.WriteLine("]");
+            
+            //cleaning time
+            //done: oem, model, announceYear
+            //to do: status, dimensions, weight, sim, type, size, res, feat, os
+            string? oem = cleanMissingValue(origs[0]);
+            string? model = cleanMissingValue(origs[1]);
+            // Console.WriteLine(origs[2]);
+            int? announceYear = cleanYear(origs[2]);
+            // Console.WriteLine(announceYear);
+            // Console.WriteLine(oem);
+            Cell c = new Cell(row.Split(","));
+            dict.Add(lineNum, c);
+        }
+        lineNum++;
+    }
+    Console.WriteLine("Done");
+}
+
+static string? cleanMissingValue(string orig) {
+    if (String.IsNullOrEmpty(orig) || orig.Equals("-")) {
+        return null;
+    } else {return orig;}
+}
+
+static string? getNext(string row, int begin) {
+    int length = 0;
+    if (row.IndexOf(",", begin) == begin) return null;
+    if (row.IndexOf("\"", begin) == begin) {
+        begin++;
+        length = row.IndexOf("\"", begin) - begin;
+    } else {
+        length = row.IndexOf(",", begin) - begin;
+        if (length < 0) length = row.Length - begin;
+    }
+    return row.Substring(begin, length);
+}
+
+static int? cleanYear(string orig) {
+    int index = orig.IndexOf(",");
+    int result = -1;
+    if (index >= 0 && int.TryParse(orig.Substring(0, index), out result)) {
+        return result;
+    }
+    return null;
+}
+
+Main(null);
