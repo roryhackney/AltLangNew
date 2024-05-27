17ced114abf8da3a4e051bf9775b0b9ef811b702
Rory Hackney <roryhackney@gmail.com>
2024-05-27 16:49:21 -0700

    Output for required questions complete

--- src/Cell.cs
+++ src/Cell.cs
@@ -1,18 +1,18 @@
 using System.Text;
 
 public class Cell {
-    private string? Oem {get; set;}
-    private string? Model {get; set;}
-    private int? LaunchAnnounced {get; set;}
-    private string? LaunchStatus {get; set;}
-    private string? BodyDimensions {get; set;}
-    private float? BodyWeight {get; set;}
-    private string? BodySim {get; set;}
-    private string? DisplayType {get; set;}
-    private float? DisplaySize {get; set;}
-    private string? DisplayResolution {get; set;}
-    private string? FeaturesSensors {get; set;}
-    private string? PlatformOS {get; set;}
+    public string? Oem {get;}
+    public string? Model {get;}
+    public int? LaunchAnnounced {get;}
+    public string? LaunchStatus {get;}
+    public string? BodyDimensions {get;}
+    public float? BodyWeight {get;}
+    public string? BodySim {get;}
+    public string? DisplayType {get;}
+    public float? DisplaySize {get;}
+    public string? DisplayResolution {get;}
+    public string? FeaturesSensors {get;}
+    public string? PlatformOS {get;}
 
     public Cell(string? Oem, string? Model, int? LaunchAnnounced, string? LaunchStatus, string? BodyDimensions,
                 float? BodyWeight, string? BodySim, string? DisplayType, float? DisplaySize, string? DisplayResolution,
--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.IO;
 using System.Numerics;
+using System.Text;
 using System.Text.RegularExpressions;
+using Microsoft.VisualBasic;
 
 namespace Program {
     public class Program {
@@ -44,11 +46,16 @@ namespace Program {
                     //create each cell and add to dict
                     Cell c = new Cell(oem, model, announceYear, launchStatus, bodyDim, weight, sim, displayType, inches, res, features, os);
                     dict.Add(lineNum, c);
-                    Console.WriteLine("Line " + lineNum + ": " + c);
+                    // Console.WriteLine("Line " + lineNum + ": " + c);
                 }
                 lineNum++;
             }
             Console.WriteLine("Done");
+            Console.WriteLine("OEM with max body weight is: " + getOemHighAvgBodyWeight(dict));
+            Console.WriteLine("\nThese models were announced in one year and released in another:");
+            Console.WriteLine(getYearDifferentOemModels(dict));
+            Console.WriteLine("There are " + numPhonesWith1Feature(dict) + " phones with 1 feature.\n");
+            Console.WriteLine("The year with the most releases after 1999 so far was " + yearMostReleases2000Up(dict));
         }
 
         public static string? cleanMissingValue(string? orig) {
@@ -153,15 +160,87 @@ namespace Program {
             return null;
         }
         
-        string getOemHighAvgBodyWeight(Dictionary<int, Cell> dict) {
-            //for each line
-            //get the oem
-            //add the body weight to the sum for that oem and increment count for that oem
-            //for each final weight, divide by count, return oem with largest
+        public static string getOemHighAvgBodyWeight(Dictionary<int, Cell> dict) {
+            //to hold the numbers
+            Dictionary<string, Tuple<float?, int>> d = new Dictionary<string, Tuple<float?, int>>();
+            //for each line get the oem
             foreach (KeyValuePair<int, Cell> entry in dict) {
-                Console.WriteLine(entry.Value);
+                if (entry.Value.Oem != null) {
+                    if (d.ContainsKey(entry.Value.Oem)) {
+                    //add the body weight to the sum for that oem and increment count for that oem
+                        float? newSum = entry.Value.BodyWeight;
+                        float? oldSum = d[entry.Value.Oem].Item1;
+                        if (newSum != null) oldSum += newSum;
+                        int newCount = d[entry.Value.Oem].Item2 + 1;
+                        d[entry.Value.Oem] = new Tuple<float?, int>(oldSum, newCount);
+                    } else {
+                        float? sum = entry.Value.BodyWeight;
+                        if (sum != null) d[entry.Value.Oem] = new Tuple<float?, int>(sum, 1);
+                    }
+                }
+            //for each final weight, divide by count, return oem with largest
             }
-            return "bees";
+            string result = "";
+            float? max = 0;
+            float? avg = 0;
+            foreach (KeyValuePair<string, Tuple<float?, int>> pair in d) {
+                if (pair.Value.Item1 != null && pair.Value.Item2 != 0) {
+                    avg = pair.Value.Item1 / pair.Value.Item2;
+                    if (avg > max) {
+                        max = avg;
+                        result = pair.Key;
+                    }
+                }
+            }
+            return result + " with an average body weight of " + avg;
+        }
+
+        public static string getYearDifferentOemModels (Dictionary<int, Cell> dict) {
+            StringBuilder sb = new StringBuilder();
+            foreach (KeyValuePair<int, Cell> pair in dict) {
+                string? ls = pair.Value.LaunchStatus;
+                //if the status starts with available
+                if (ls != null && ls.StartsWith("Av")) {
+                    int? year = cleanYear(ls);
+                    //and the announceYear != status year extracted
+                    if (year != null && year != pair.Value.LaunchAnnounced) {
+                        //add oem and model and newline to result
+                        sb.Append(pair.Value.Oem).Append(' ').Append(pair.Value.Model).Append('\n');
+                    }
+                }
+            }
+            return sb.ToString();
+        }
+
+        public static int numPhonesWith1Feature(Dictionary<int, Cell> dict) {
+            int count = 0;
+            foreach (KeyValuePair<int, Cell> pair in dict) {
+                string? features = pair.Value.FeaturesSensors;
+                if (features != null && ! features.Contains(',')) {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public static int yearMostReleases2000Up(Dictionary<int, Cell> dict) {
+            Dictionary<int, int> yearCount = new Dictionary<int, int>();
+            foreach (KeyValuePair<int, Cell> pair in dict) {
+                string? status = pair.Value.LaunchStatus;
+                if (status != null && status.StartsWith("Av")) {
+                    int? year = cleanYear(status);
+                    if (year != null && year > 1999) {
+                        if (yearCount.ContainsKey((int)year)) yearCount[(int)year]++;
+                        else {yearCount[(int)year] = 1;}
+                    }
+                }
+            }
+            int max = 0;
+            int topYear = 0;
+            foreach (KeyValuePair<int, int> entry in yearCount) {
+                if (entry.Value > max) topYear = entry.Key;
+            }
+            return topYear;
         }
     }
 
