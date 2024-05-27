99c2fb6ae7a18753270d5ab7aef3b2d193f1d98f
Rory Hackney <roryhackney@gmail.com>
2024-05-27 10:11:53 -0700

    setup files

---
+++ src/Example.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AltLang.Example
+{
+    public class NewClass
+    {
+        public bool NewMethod(bool modifier)
+        {
+            return modifier;
+        }
+    }
+}
