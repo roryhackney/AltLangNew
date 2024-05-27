be224e8d307e6d1df0d9025876d14312286db0fc
Rory Hackney <roryhackney@gmail.com>
2024-05-27 14:50:32 -0700

    file reading done

--- src/Cell.cs
+++ src/Cell.cs
@@ -1,18 +1,51 @@
+using System.Text;
+
 public class Cell {
     private string? Oem {get; set;}
-    private string Model {get; set;}
-    private int LaunchAnnounced {get; set;}
-    private string LaunchStatus {get; set;}
-    private string BodyDimensions {get; set;}
-    private float BodyWeight {get; set;}
-    private string BodySim {get; set;}
-    private string DisplayType {get; set;}
-    private float DisplaySize {get; set;}
-    private string DisplayResolution {get; set;}
-    private string FeaturesSensors {get; set;}
-    private string PlatformOS {get; set;}
+    private string? Model {get; set;}
+    private int? LaunchAnnounced {get; set;}
+    private string? LaunchStatus {get; set;}
+    private string? BodyDimensions {get; set;}
+    private float? BodyWeight {get; set;}
+    private string? BodySim {get; set;}
+    private string? DisplayType {get; set;}
+    private float? DisplaySize {get; set;}
+    private string? DisplayResolution {get; set;}
+    private string? FeaturesSensors {get; set;}
+    private string? PlatformOS {get; set;}
 
-    public Cell(string[] values) {
-    
+    public Cell(string? Oem, string? Model, int? LaunchAnnounced, string? LaunchStatus, string? BodyDimensions,
+                float? BodyWeight, string? BodySim, string? DisplayType, float? DisplaySize, string? DisplayResolution,
+                string? FeaturesSensors, string? PlatformOS) {
+                    this.Oem = Oem;
+                    this.Model = Model;
+                    this.LaunchAnnounced = LaunchAnnounced;
+                    this.LaunchStatus = LaunchStatus;
+                    this.BodyDimensions = BodyDimensions;
+                    this.BodyWeight = BodyWeight;
+                    this.BodySim = BodySim;
+                    this.DisplayType = DisplayType;
+                    this.DisplaySize = DisplaySize;
+                    this.DisplayResolution = DisplayResolution;
+                    this.FeaturesSensors = FeaturesSensors;
+                    this.PlatformOS = PlatformOS;
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder("{");
+        sb.Append(Oem).Append("\n");
+        sb.Append(Model).Append("\n");
+        sb.Append(LaunchAnnounced).Append("\n");
+        sb.Append(LaunchStatus).Append("\n");
+        sb.Append(BodyDimensions).Append("\n");
+        sb.Append(BodyWeight).Append("\n");
+        sb.Append(BodySim).Append("\n");
+        sb.Append(DisplayType).Append("\n");
+        sb.Append(DisplaySize).Append("\n");
+        sb.Append(DisplayResolution).Append("\n");
+        sb.Append(FeaturesSensors).Append("\n");
+        sb.Append(PlatformOS).Append("}").Append("\n");
+        return sb.ToString();
     }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System.IO;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
 namespace Program {
     public class Program {
 
         static void Main(string[] args) {
             Dictionary<int, Cell> dict = new Dictionary<int, Cell>();
             //step one: read the file
-            IEnumerable<string> rows = File.ReadLines("cells.csv");
+            IEnumerable<string> rows = File.ReadLines("src/cells.csv");
             int lineNum = 1;
             //step two: create the Cell objects
             foreach (string row in rows) {
@@ -23,29 +26,32 @@ namespace Program {
                         begin += length;
                         if (origs[index] != null) begin += origs[index].Length;
                     }            
-                    // Console.WriteLine(row);
-                    Console.WriteLine("[");
-                    foreach (string item in origs) {Console.WriteLine(item + "---");}
-                    Console.WriteLine("]");
                     
                     //cleaning time
-                    //done: oem, model, announceYear
-                    //to do: status, dimensions, weight, sim, type, size, res, feat, os
                     string? oem = cleanMissingValue(origs[0]);
                     string? model = cleanMissingValue(origs[1]);
-                    // Console.WriteLine(origs[2]);
                     int? announceYear = cleanYear(origs[2]);
-                    // Console.WriteLine(announceYear);
-                    // Console.WriteLine(oem);
-                    Cell c = new Cell(row.Split(","));
+                    string? launchStatus = cleanStatus(origs[3]);
+                    string? bodyDim = cleanMissingValue(origs[4]);
+                    float? weight = cleanWeight(origs[5]);
+                    string? sim = cleanYesNo(cleanMissingValue(origs[6]));
+                    string? displayType = cleanMissingValue(origs[7]);
+                    float? inches = cleanSize(origs[8]);
+                    string? res = cleanMissingValue(origs[9]);
+                    string? features = cleanNums(origs[10]);
+                    string? os = cleanOS(origs[11]);
+
+                    //create each cell and add to dict
+                    Cell c = new Cell(oem, model, announceYear, launchStatus, bodyDim, weight, sim, displayType, inches, res, features, os);
                     dict.Add(lineNum, c);
+                    Console.WriteLine("Line " + lineNum + ": " + c);
                 }
                 lineNum++;
             }
             Console.WriteLine("Done");
         }
 
-        public static string? cleanMissingValue(string orig) {
+        public static string? cleanMissingValue(string? orig) {
             if (orig != null) {
                 string trimmed = orig.Trim();
                 if (trimmed.Equals("-") || trimmed.Equals("")) return null;
@@ -67,13 +73,96 @@ namespace Program {
             return row.Substring(begin, length);
         }
 
-        public static int? cleanYear(string orig) {
-            int index = orig.IndexOf(',');
-            int result = -1;
-            if (index >= 0 && int.TryParse(orig.Substring(0, index), out result)) {
-                return result;
+        public static int? cleanYear(string? orig) {
+            if (orig != null) {
+                string pattern = "[0-9]{4}";
+                if (Regex.IsMatch(orig, pattern)) {
+                    return int.Parse(Regex.Match(orig, pattern).Value);
+                }
             }
             return null;
         }
+
+        public static string? cleanStatus(string? orig) {
+            if (orig != null) {
+                string? result = orig.Trim();
+                if (result.Equals("Discontinued") || result.Equals("Canceled")) {
+                    return result;
+                } else {
+                    string reg = "Available. Released [0-9]{4}";
+                    if (Regex.IsMatch(result, reg)) {
+                        return result;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public static float? cleanWeight(string? orig) {
+            if (orig != null) {
+                int endIndex = orig.IndexOf('g');
+                if (endIndex > 0) {
+                    string result = orig.Substring(0, endIndex).Trim();
+                    float f;
+                    try {
+                        f = float.Parse(result);
+                    } catch (Exception) {
+                        return null;
+                    }
+                    return f;
+                }
+            }
+            return null;
+        }
+        
+        public static string? cleanYesNo(string? orig) {
+            if (orig != null) {
+                if (! orig.Equals("Yes") && ! orig.Equals("No")) return orig;
+            }
+            return null;
+        }
+        
+        public static float? cleanSize(string? orig) {
+            if (orig != null) {
+                string pattern = "[0-9]+.[0-9]+ inches";
+                if (Regex.IsMatch(orig, pattern)) {
+                    Match m = Regex.Match(orig, pattern);
+                    string result = m.Value.Substring(0, m.Value.IndexOf("inches")).Trim();
+                    return float.Parse(result);
+                }
+            }
+            return null;
+        }
+
+        public static string? cleanNums(string? orig) {
+            if (orig != null) {
+                string? result = cleanMissingValue(orig);
+                if (result != null && Regex.IsMatch(result, "[a-zA-Z]")) {
+                    return result;
+                }
+            }
+            return null;
+        }
+
+        public static string? cleanOS(string? orig) {
+            if (orig != null) {
+                int commaIdx = orig.IndexOf(',');
+                if (commaIdx < 0) return orig;
+                else {return orig[..commaIdx].Trim();}
+            }
+            return null;
+        }
+        
+        string getOemHighAvgBodyWeight(Dictionary<int, Cell> dict) {
+            //for each line
+            //get the oem
+            //add the body weight to the sum for that oem and increment count for that oem
+            //for each final weight, divide by count, return oem with largest
+            foreach (KeyValuePair<int, Cell> entry in dict) {
+                Console.WriteLine(entry.Value);
+            }
+            return "bees";
+        }
     }
+
 }
